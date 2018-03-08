035f981807fd469dd17cf5a0e23416498d5b7744
dasion <824707315@qq.com>
2018-03-08 12:34:03 +0800

    方法抽取

--- src/main/java/com/dasion/daydayfund/fund/DayDayFundBaseInfo.java
+++ src/main/java/com/dasion/daydayfund/fund/DayDayFundBaseInfo.java
@@ -34,39 +34,26 @@ public class DayDayFundBaseInfo implements Runnable {
 	}
 
 	public void getFundBaseinfo() throws IOException, InterruptedException {
-		HttpclientTool tool = new HttpclientTool();
-		tool.createDefaultConfigClientByHttpClients();
-		tool.setDefaultCookieStore();
-		tool.setDefaultHeader();
+		HttpclientTool tool = getHttpClient();
 		tool.setUrl(url);
 		String fundListHtml = tool.getContentByGet();
 		tool.getClient().close();
 		Document fundListDoc = Jsoup.parse(fundListHtml);
 		Elements fundListTable = fundListDoc.getElementsByClass("ttjj-table ttjj-table-hover common-sort-table");
-		Gson gson = new Gson();
+
 		if (fundListTable.size() > 0) {
 			Elements fundListTrs = fundListTable.get(0).getElementsByTag(HtmlConstant.TBODY).get(0).getElementsByTag(HtmlConstant.TR);
 			for (Element fundTr : fundListTrs) {
-				FundBean fund = new FundBean();
 				Elements fundTds = fundTr.getElementsByTag(HtmlConstant.TD);
 
 				if(fundTds.get(0).getElementsByTag(HtmlConstant.A).size() < 1){
 					continue;
 				}
-				fund.setFundCode(fundTds.get(0).getElementsByTag(HtmlConstant.A).get(1).text());
-				fund.setFundName(fundTds.get(0).getElementsByTag(HtmlConstant.A).get(0).text());
-				fund.setFundType(fundTds.get(2).text());
-
-				fund.setHalfyearInc(format(fundTds.get(7).text().trim()));
-				fund.setMonthInc(format(fundTds.get(5).text().trim()));
-				fund.setSeasonhInc(format(fundTds.get(6).text().trim()));
-				fund.setSetupInc(format(fundTds.get(11).text().trim()));
-				fund.setThisyearInc(format(fundTds.get(10).text().trim()));
-				fund.setTwoyearInc(format(fundTds.get(9).text().trim()));
-				fund.setWeekInc(format(fundTds.get(4).text().trim()));
-				fund.setYearInc(format(fundTds.get(8).text().trim()));
+				
+				FundBean fund = concatFundBean(fundTds);
 
 				try (Jedis jedis = JedisTool.getInstance().getResource()) {
+					Gson gson = new Gson();
 					jedis.lpush(taskQueue, gson.toJson(fund));
 				}
 			}
@@ -74,6 +61,30 @@ public class DayDayFundBaseInfo implements Runnable {
 				jedis.set(RedisConstant.SEMAPHORE_KEY, RedisConstant.SEMAPHORE_KEY_RUN);
 			}
 		}
+	}
+
+	private FundBean concatFundBean(Elements fundTds) {
+		FundBean fund = new FundBean();
+		fund.setFundCode(fundTds.get(0).getElementsByTag(HtmlConstant.A).get(1).text());
+		fund.setFundName(fundTds.get(0).getElementsByTag(HtmlConstant.A).get(0).text());
+		fund.setFundType(fundTds.get(2).text());
+		fund.setHalfyearInc(format(fundTds.get(7).text().trim()));
+		fund.setMonthInc(format(fundTds.get(5).text().trim()));
+		fund.setSeasonhInc(format(fundTds.get(6).text().trim()));
+		fund.setSetupInc(format(fundTds.get(11).text().trim()));
+		fund.setThisyearInc(format(fundTds.get(10).text().trim()));
+		fund.setTwoyearInc(format(fundTds.get(9).text().trim()));
+		fund.setWeekInc(format(fundTds.get(4).text().trim()));
+		fund.setYearInc(format(fundTds.get(8).text().trim()));
+		return fund;
+	}
+
+	private HttpclientTool getHttpClient() {
+		HttpclientTool tool = new HttpclientTool();
+		tool.createDefaultConfigClientByHttpClients();
+		tool.setDefaultCookieStore();
+		tool.setDefaultHeader();
+		return tool;
 	}
 
 	private String format(String text) {
